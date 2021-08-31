16ec3bb19abab0e9eb692214c087d0535fc5aa9c
ktmrose <katie@getvozzi.com>
2021-08-31 15:48:31 -0600

    Testing manual rejection

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Message changed to test demo CodePipeline!'));
+app.get('/', (req, res) => res.send('This modification should be rejected.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
